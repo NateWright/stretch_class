59bf398ee120b1a64954491bbac416379fe7d62a
nwright <nathanwrightbusiness@gmail.com>
2023-01-30 18:57:35 -0500

    added multiple tf frames

--- stretch_tf/src/stretch_tf.cpp
+++ stretch_tf/src/stretch_tf.cpp
@@ -10,14 +10,20 @@ Stretch_tf::~Stretch_tf() {
 }
 
 void Stretch_tf::clickHandlerCallback(const geometry_msgs::PointStampedPtr& msg) {
-    try {
-        ROS_INFO_STREAM("\nOriginal Message\n"
-                        << *msg);
-
-        auto newMsg = tfBuffer_.transform(*msg, "base_link", ros::Duration(0.1));
-        ROS_INFO_STREAM("\nNew Message\n"
-                        << newMsg);
-    } catch (...) {
-        ROS_ERROR_STREAM("Unable to perform Transform");
+    std::string list[] = {
+        "base_link",
+        "odom",
+        "camera_depth_optical_frame",
+        "link_gripper"};
+    ROS_INFO_STREAM("\nOriginal Message\n"
+                    << *msg);
+    for (std::string str : list) {
+        try {
+            auto newMsg = tfBuffer_.transform(*msg, str, ros::Duration(0.1));
+            ROS_INFO_STREAM("\nNew Message\n"
+                            << newMsg);
+        } catch (...) {
+            ROS_ERROR_STREAM("Unable to perform Transform");
+        }
     }
 }
